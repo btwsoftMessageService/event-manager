4d849ca1ce610aeec730c6dfcfc4882434015729
kh <pkh@btwsoft.com>
2026-04-07 09:25:46 +0900

    - config 수정, 에러 메시지 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    allowedDevOrigins: ["127.0.0.1", "localhost", "192.168.0.227"]
+    /* config options here */
 };
 
 export default nextConfig;
